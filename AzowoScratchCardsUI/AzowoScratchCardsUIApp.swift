8e3ee33f3ef6918857da77ef7de0a7b6837bed27
Marek Baláž <marek.balaz@casperise.com>
2023-07-25 00:19:57 +0200

    Initial Commit

---
+++ AzowoScratchCardsUI/AzowoScratchCardsUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AzowoScratchCardsUIApp.swift
+//  AzowoScratchCardsUI
+//
+//  Created by Marek Baláž on 25/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct AzowoScratchCardsUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
